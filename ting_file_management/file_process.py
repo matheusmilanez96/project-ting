9f4df32f570b970e5e23e9a1ed070d06b69c2607
Matheus Milanez <mattmila96@gmail.com>
2023-08-29 16:02:56 -0300

    requisito 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -29,4 +29,10 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        file = instance.search(position)
+
+        print(f"{file}")
+
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
