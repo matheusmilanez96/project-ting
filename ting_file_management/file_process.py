f61abe327d2825cfdd50f9f6f2db8c9787213e0d
Matheus Milanez <mattmila96@gmail.com>
2023-08-29 15:03:26 -0300

    requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for file in range(len(instance)):
+        if instance.search(file)["nome_do_arquivo"] == path_file:
+            return None
+
+    content = txt_importer(path_file)
+
+    output = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content),
+        "linhas_do_arquivo": content
+    }
+
+    instance.enqueue(output)
+    sys.stdout.write(f"{output}")
 
 
 def remove(instance):
