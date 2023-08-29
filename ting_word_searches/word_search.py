fa5905d7a6860f1d5c233bce7ace053d6475d412
Matheus Milanez <mattmila96@gmail.com>
2023-08-29 16:51:41 -0300

    requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    found_instances = []
+    for i in range(len(instance)):
+        ocorrencias = []
+        file = instance.search(i)
+        index = 1
+        for line in file["linhas_do_arquivo"]:
+            if word.lower() in line.lower():
+                ocorrencias.append({"linha": index})
+            index += 1
+        if ocorrencias != []:
+            data = {
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": ocorrencias
+            }
+            found_instances.append(data)
+    return found_instances
 
 
 def search_by_word(word, instance):
